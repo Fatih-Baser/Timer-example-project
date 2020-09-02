a8fa2fce3e62e9e6f7d98de535d5480f0593b9d0
fatih40 <baserfatih40@gmail.com>
2020-09-02 08:47:22 +0200

    yuklendi

---
+++ app/src/main/java/com/example/zamanlayiciornekporje/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.zamanlayiciornekporje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView progressText;
+    int i=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.progress_bar);
+        progressText=findViewById(R.id.progress_text);
+
+        final Handler  handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(i<=100) {
+                    progressText.setText("" + i);
+                    progressBar.setProgress(i);
+                    i++;
+                    handler.postDelayed(this, 200);
+                }else{
+                    handler.removeCallbacks(this);
+                }
+
+
+            }
+        },200);
+
+    }
+}
